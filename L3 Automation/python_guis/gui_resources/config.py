ea1d962b508f95f9ea5554fe52a1ee5b8c76e53e
Antoni Lenart <antek@lenart.org.pl>
2024-03-18 21:19:16 +0100

    removed some properties from main to config

--- L3 Automation/python_guis/gui_resources/config.py
+++ L3 Automation/python_guis/gui_resources/config.py
@@ -2,5 +2,11 @@ APPNAME = 'appname'
 VERSION = '0.0.1'
 
 BG_COLOR = 'grey'
+FONT = 'Segoe UI'
+FONTSIZE = 12
 
+WIDTH = 800
+HEIGHT = 800
 
+WINDOW_ICON_PATH = 'gui_resources/APP_ICON_512.png'
+QUIT_ICON_PATH = 'gui_resources/QUIT_512.png'
