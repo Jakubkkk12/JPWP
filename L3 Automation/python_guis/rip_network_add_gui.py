a53bc7f6a4e6eea63d28c0a3c006d6d1b20911f0
Antoni Lenart <antek@lenart.org.pl>
2024-04-06 20:45:21 +0200

    added rip networks configuration

---
+++ L3 Automation/python_guis/rip_network_add_gui.py
@@ -0,0 +1,107 @@
+import tkinter as tk
+from tkinter import messagebox
+
+from resources.devices.Router import Router
+from gui_resources import config
+from resources.routing_protocols.Network import Network
+
+
+class RIPNetworkAddGUI:
+    def __init__(self, router: Router, rip_networks_gui):
+        self.hostname = router.name
+        self.rip_networks_gui = rip_networks_gui
+
+        root = tk.Toplevel()
+        # ######## WINDOW PARAMETERS ######## #
+        # title
+        root.title(config.APPNAME + ' ' + config.VERSION + ' ' + self.hostname + ' Add RIP Network')
+
+        # window icon, using conversion to iso, cause tkinter doesn't accept jpg
+        icon = tk.PhotoImage(file=config.WINDOW_ICON_PATH)
+        root.wm_iconphoto(False, icon)
+
+        # size parameters
+        width = 400
+        height = 300
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.geometry(alignstr)
+        root.resizable(width=True, height=True)
+
+        root.minsize(300, 300)
+
+        lblNetwork = tk.Label(root, text='Network:')
+
+        NetworkFrame = tk.Frame(root)
+        entryIPNetworkFirst = tk.Entry(NetworkFrame, width=10)
+        entryIPNetworkFirst.pack(side='left')
+        labelDot1 = tk.Label(NetworkFrame, text='.')
+        labelDot1.pack(side='left')
+
+        entryIPNetworkSecond = tk.Entry(NetworkFrame, width=10)
+        entryIPNetworkSecond.pack(side='left')
+        labelDot2 = tk.Label(NetworkFrame, text='.')
+        labelDot2.pack(side='left')
+
+        entryIPNetworkThird = tk.Entry(NetworkFrame, width=10)
+        entryIPNetworkThird.pack(side='left')
+        labelDot3 = tk.Label(NetworkFrame, text='.')
+        labelDot3.pack(side='left')
+
+        entryIPNetworkFourth = tk.Entry(NetworkFrame, width=10)
+        entryIPNetworkFourth.pack(side='left')
+
+        lblMask = tk.Label(root, text='Mask:')
+        entryMask = tk.Entry(root, width=5)
+
+        lblNetwork.pack()
+        NetworkFrame.pack()
+        lblMask.pack()
+        entryMask.pack()
+
+        def get_network() -> str:
+            return (entryIPNetworkFirst.get() + '.' + entryIPNetworkSecond.get() + '.' +
+                    entryIPNetworkThird.get() + '.' + entryIPNetworkFourth.get())
+
+        def get_mask() -> int:
+            return int(entryMask.get())
+
+        def clean_entries() -> None:
+            ip_entries = [entryIPNetworkFirst, entryIPNetworkSecond, entryIPNetworkThird,
+                          entryIPNetworkFourth]
+            for entry in ip_entries:
+                entry.delete(0, 'end')
+            entryMask.delete(0, 'end')
+
+        def validate_network() -> bool:
+            ip_entries = [entryIPNetworkFirst, entryIPNetworkSecond, entryIPNetworkThird, entryIPNetworkFourth]
+            for entry in ip_entries:
+                value = entry.get()
+                if not value.isdigit() or not (0 <= int(value) <= 255):
+                    messagebox.showerror('Error', 'Incorrect IP Format', parent=root)
+                    clean_entries()
+                    return False
+            value = entryMask.get()
+            if not value.isdigit() or not (0 <= int(value) <= 32):
+                messagebox.showerror('Error', 'Incorrect mask value', parent=root)
+                entryMask.delete(0, 'end')
+                return False
+            return True
+
+        def apply_network():
+            if validate_network():
+                messagebox.showinfo('Network added', 'Network added', parent=root)
+
+                network = get_network()
+                mask = get_mask()
+                network = Network(network=network, mask=mask)
+                self.rip_networks_gui.insert_network(network)
+                clean_entries()
+
+        btnApply = tk.Button(root, text='Apply', command=apply_network)
+        btnApply.pack(pady=5)
+        btnQuit = tk.Button(root, text='Quit', command=root.destroy)
+        btnQuit.pack()
+
+        root.mainloop()
---
+++ L3 Automation/python_guis/rip_networks_gui.py
@@ -0,0 +1,91 @@
+import tkinter as tk
+import tkinter.ttk
+from gui_resources import config
+from python_guis.ospf_network_add_gui import OSPFNetworkAddGUI
+from python_guis.rip_network_add_gui import RIPNetworkAddGUI
+from resources.devices.Router import Router
+from resources.routing_protocols.ospf.OSPFArea import OSPFArea
+from resources.routing_protocols.Network import Network
+
+
+class RIPNetworksGUI:
+    def __init__(self, router: Router):
+        root = tk.Toplevel()
+
+        # title
+        root.title(config.APPNAME + ' ' + config.VERSION + ' ' + router.name + ' RIP Networks Configuration')
+        # window icon, using conversion to iso, cause tkinter doesn't accept jpg
+        icon = tk.PhotoImage(file=config.WINDOW_ICON_PATH)
+        root.wm_iconphoto(False, icon)
+
+        # size parameters
+        width = 400
+        height = 400
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.geometry(alignstr)
+        root.resizable(width=True, height=True)
+
+        root.minsize(400, 400)
+
+        root.grid_columnconfigure(1, weight=1)
+        root.grid_columnconfigure(1, weight=1)
+
+        treeFrame = tk.Frame(root)
+        scrollbar = tk.Scrollbar(treeFrame, orient='vertical')
+        scrollbar.pack(side='right', fill='y')
+
+        treeColumns = ('No', 'Network', 'Mask')
+        self.tree = tk.ttk.Treeview(treeFrame, columns=treeColumns, show='headings')
+        self.tree.configure(yscrollcommand=scrollbar.set)
+        self.tree.pack(side='left', fill='both', expand=True)
+
+        scrollbar.config(command=self.tree.yview)
+
+        self.tree.heading(treeColumns[0], text='No', anchor='w')
+        self.tree.column(treeColumns[0], width=15)
+
+        self.tree.heading(treeColumns[1], text='Network', anchor='w')
+        self.tree.column(treeColumns[1], width=50)
+
+        self.tree.heading(treeColumns[2], text='Mask', anchor='w')
+        self.tree.column(treeColumns[2], width=30)
+
+        i = 1
+        for k, network in router.rip.networks.items():
+            values = (i, network.network, network.mask)
+            self.tree.insert('', tk.END, iid=i-1, values=values)
+            i += 1
+
+        treeFrame.grid(column=0, row=3, columnspan=2, sticky='NEWS')
+
+        def add_network(router, self):
+            RIPNetworkAddGUI(router, self)
+
+        def remove_network() -> None:
+            item = self.tree.selection()
+            self.tree.delete(item)
+            # Update No
+            children = self.tree.get_children()
+            for i, child in enumerate(children, start=1):
+                self.tree.item(child, values=(i,) + self.tree.item(child, 'values')[1:])
+
+        buttonFrame = tk.Frame(root)
+        btnAdd = tk.Button(buttonFrame, text='Add', command=lambda: add_network(router, self))
+        btnAdd.pack()
+        btnRemove = tk.Button(buttonFrame, text='Remove', command=remove_network)
+        btnRemove.pack()
+        btnQuit = tk.Button(buttonFrame, text='Quit', command=root.destroy)
+        btnQuit.pack()
+        buttonFrame.grid(column=0, row=4, columnspan=2)
+
+        root.mainloop()
+
+    def insert_network(self, network: Network):
+        last_item = self.tree.get_children()[-1]
+        last_index = self.tree.index(last_item)
+        no = last_index + 2
+        values = (no, network.network, network.mask)
+        # Update tree
+        self.tree.insert('', tk.END, values=values)
