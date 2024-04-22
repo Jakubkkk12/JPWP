c8a05b711bd661187d65578d70df9cd090112e21
Antoni Lenart <antek@lenart.org.pl>
2024-04-22 11:41:45 +0200

    removed login, added it as separate button

--- L3 Automation/python_guis/gui_resources/config.py
+++ L3 Automation/python_guis/gui_resources/config.py
@@ -10,3 +10,4 @@ HEIGHT = 800
 
 WINDOW_ICON_PATH = 'gui_resources/APP_ICON_512.png'
 QUIT_ICON_PATH = 'gui_resources/QUIT_512.png'
+LOGIN_ICON_PATH = 'gui_resources/LOGIN_512.png'
--- L3 Automation/python_guis/login_gui.py
+++ L3 Automation/python_guis/login_gui.py
@@ -6,13 +6,13 @@ APPNAME = config.APPNAME
 VERSION = config.VERSION
 BG_COLOR = config.BG_COLOR
 
-WINDOW_ICON_PATH = 'gui_resources/APP_ICON_512.png'
-LOGIN_ICON_PATH = 'gui_resources/LOGIN_512.png'
+WINDOW_ICON_PATH = config.WINDOW_ICON_PATH
+LOGIN_ICON_PATH = config.LOGIN_ICON_PATH
 
 
 class LoginGUI:
     def __init__(self):
-        self.root = tk.Tk()
+        self.root = tk.Toplevel()
         # ######## WINDOW PARAMETERS ######## #
         # title
         self.root.title(APPNAME + ' ' + VERSION + ' Login')
@@ -35,7 +35,6 @@ class LoginGUI:
         # background color
         self.root.configure(bg=BG_COLOR)
 
-        # ######## FUNCTIONS ######## #
         top_margin = tk.Frame(self.root, bg=BG_COLOR, height=10)
         top_margin.pack()
 
@@ -51,37 +50,23 @@ class LoginGUI:
         self.entryPassword = tk.Entry(self.root, show='*')
         self.entryPassword.pack(fill='x', padx=50)
 
-        lblSSHPassword = tk.Label(self.root, text='SSH password (optional):', bg=BG_COLOR)
-        lblSSHPassword.pack()
-
-        self.entrySSHPassword = tk.Entry(self.root, show='*')
-        self.entrySSHPassword.pack(fill='x', padx=50)
-
         LOGIN_ICON = Image.open(LOGIN_ICON_PATH)
         LOGIN_ICON = LOGIN_ICON.resize((24, 24))
         tk_icon = ImageTk.PhotoImage(LOGIN_ICON)
-        btnLogin = tk.Button(self.root, text='Login', image=tk_icon, compound=tk.RIGHT, command=self.validate_credentials)
+        btnLogin = tk.Button(self.root, text='Login', image=tk_icon, compound=tk.RIGHT,
+                             command=self.validate_credentials)
         btnLogin.pack(pady=5)
 
-        btnQuit = tk.Button(self.root, text='Quit', command=self.root.destroy)
-        btnQuit.pack(pady=5)
+        btnCancel = tk.Button(self.root, text='Cancel', command=self.root.destroy)
+        btnCancel.pack(pady=5)
 
         self.root.mainloop()
 
     def validate_credentials(self):
         username = self.usernameEntry.get()
         password = self.entryPassword.get()
-        sshPassword = self.entrySSHPassword.get()
         self.root.destroy()
 
         # import has to be there to avoid circular imports
         from main_gui import MainGUI
         MainGUI()
-
-         # if correct:
-         #    run maingui
-         # else error
-
-
-if __name__ == '__main__':
-    LoginGUI()
